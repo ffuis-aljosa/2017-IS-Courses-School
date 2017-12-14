b528fc63ee2154e9af123f180ca98d3eaa859a5f
bojanamilacic <33426252+bojanamilacic@users.noreply.github.com>
2017-12-14 17:45:12 +0100

    Database Connection

---
+++ Courses School/DbConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+
+namespace Courses_School
+{
+
+     class DbConnection
+        {
+            private SqlCeConnection connection;
+
+            private static DbConnection instance;
+
+            private DbConnection(string connectionString)
+            {
+                connection = new SqlCeConnection(connectionString);
+                connection.Open();
+            }
+
+            public static DbConnection Instance
+            {
+                get
+                {
+                    if (instance == null)
+                        instance = new DbConnection(@"Data Source=..\..\CoursesSchoolDB.sdf");
+
+                    return instance;
+                }
+            }
+
+            public SqlCeConnection Connection
+            {
+                get
+                {
+                    return connection;
+                }
+            }
+        }
+    }
+
+
+
