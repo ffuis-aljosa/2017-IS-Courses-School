3724fe687b2046ca7a5b14455c4583c0a802aebe
SuncicaMaric <33877323+SuncicaMaric@users.noreply.github.com>
2017-12-26 20:14:28 +0100

    Class "SchoolSubjects"

---
+++ Courses School/Models/SchoolSubjects.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+namespace Courses_School.Models
+{
+    class SchoolSubjects
+    {
+        private int id;
+        private string school_subject;
+        private int number_of_classes;
+        private Emloyees employee;
+
+        public SchoolSubjects(int id, string school_subject, int number_of_classes, Emloyees employee)
+
+        {
+            this.id = id;
+            School_subject = school_subject;
+            Number_of_classes = number_of_classes;
+            Employee = employee;
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public string School_subject
+        {
+            get
+            {
+                return school_subject;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Naziv predmeta ne može biti prazan!");
+
+                school_subject = value;
+            }
+        }
+
+        public int Number_of_classes
+        {
+            get
+            {
+                return number_of_classes;
+            }
+            set
+            {
+                if (value < 1)
+                    throw new Exception("Broj časova mora biti veći od 1.");
+
+                if (value > 100)
+                    throw new Exception("Broj časova mora biti manji od 100.");
+
+                number_of_classes = value;
+            }
+
+        }
+
+        public Emloyees Employee
+        {
+
+            get
+            {
+                return employee;
+            }
+            set
+            {
+                employee = value;
+            }
+
+        }
+        
+    }
+}
