c65e2346493054d6f0904565356b108b86c12bdc
SuncicaMaric <33877323+SuncicaMaric@users.noreply.github.com>
2018-02-07 17:25:58 +0100

    SchoolTimeTableForm

--- Courses School/schoolTimetableForm.Designer.cs
+++ Courses School/schoolTimetableForm.Designer.cs
@@ -28,19 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.classroom1Button = new System.Windows.Forms.Button();
+            this.classroom2Botton = new System.Windows.Forms.Button();
+            this.classroom3Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // classroom1Button
+            // 
+            this.classroom1Button.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.classroom1Button.Location = new System.Drawing.Point(74, 51);
+            this.classroom1Button.Name = "classroom1Button";
+            this.classroom1Button.Size = new System.Drawing.Size(387, 37);
+            this.classroom1Button.TabIndex = 0;
+            this.classroom1Button.Text = "Učionica br.1";
+            this.classroom1Button.UseVisualStyleBackColor = false;
+            this.classroom1Button.Click += new System.EventHandler(this.classroom1Button_Click);
+            // 
+            // classroom2Botton
+            // 
+            this.classroom2Botton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.classroom2Botton.Location = new System.Drawing.Point(74, 130);
+            this.classroom2Botton.Name = "classroom2Botton";
+            this.classroom2Botton.Size = new System.Drawing.Size(387, 36);
+            this.classroom2Botton.TabIndex = 1;
+            this.classroom2Botton.Text = "Učionica br.2";
+            this.classroom2Botton.UseVisualStyleBackColor = false;
+            this.classroom2Botton.Click += new System.EventHandler(this.classroom2Botton_Click);
+            // 
+            // classroom3Button
+            // 
+            this.classroom3Button.Location = new System.Drawing.Point(74, 211);
+            this.classroom3Button.Name = "classroom3Button";
+            this.classroom3Button.Size = new System.Drawing.Size(387, 34);
+            this.classroom3Button.TabIndex = 2;
+            this.classroom3Button.Text = "Učionica br.3";
+            this.classroom3Button.UseVisualStyleBackColor = true;
+            this.classroom3Button.Click += new System.EventHandler(this.classroom3Button_Click);
             // 
             // schoolTimetableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Courses_School.Properties.Resources.pozadina;
             this.ClientSize = new System.Drawing.Size(560, 319);
+            this.Controls.Add(this.classroom3Button);
+            this.Controls.Add(this.classroom2Botton);
+            this.Controls.Add(this.classroom1Button);
             this.Name = "schoolTimetableForm";
-            this.Text = "schoolTimetableForm";
+            this.Text = "Raspored ";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button classroom1Button;
+        private System.Windows.Forms.Button classroom2Botton;
+        private System.Windows.Forms.Button classroom3Button;
     }
 }
--- Courses School/schoolTimetableForm.cs
+++ Courses School/schoolTimetableForm.cs
@@ -16,5 +16,25 @@ namespace Courses_School
         {
             InitializeComponent();
         }
+
+        private void classroom1Button_Click(object sender, EventArgs e)
+        {
+            classroom1Form classroom1 = new classroom1Form();
+            classroom1.Show();
+        }
+
+        private void classroom2Botton_Click(object sender, EventArgs e)
+        {
+            classroom2Form classroom2 = new classroom2Form();
+            classroom2.Show();
+
+        }
+
+        private void classroom3Button_Click(object sender, EventArgs e)
+        {
+            classroom3Form classroom3 = new classroom3Form();
+            classroom3.Show();
+
+        }
     }
 }
